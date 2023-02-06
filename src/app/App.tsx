3c33f82dbda3c2c1bc9c23460c86929d5bf03cff
ipixels02 <ar4iking19@gmail.com>
2023-02-06 16:56:15 +0500

    add FSD as Architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import Counter from "./components/Counter";
 import './styles/index.scss';
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import AboutPage from 'pages/AboutPage/ui/AboutPage';
+import MainPage from "pages/MainPage/ui/MainPage";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -16,11 +15,10 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Загрузка...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
-            <Counter/>
         </div>
     );
 };
