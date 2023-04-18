7a9d4a190dc5104d0e4128737e307d52378a257b
ipixels02 <ar4iking19@gmail.com>
2023-04-18 22:20:11 +0500

    feat: add modal, portal, change header (18.04.2023, 22:18)

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
     plugins: [
         'react',
         '@typescript-eslint',
-        'i18next'
+        'i18next',
+        'react-hooks'
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -55,16 +56,19 @@ module.exports = {
         semi: 'off',
         '@typescript-eslint/semi': 'off',
         '@typescript-eslint/consistent-type-imports': 'off',
-        '@typescript-eslint/consistent-type-assertions': 'off'
+        '@typescript-eslint/consistent-type-assertions': 'off',
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'error' // Checks effect dependencies
     },
     globals: {
         __IS_DEV__: true
     },
     overrides: [
         {
-            files: ['**/src/**/*.test.{ts,tsx}'],
+            files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
             rules: {
-                'i18next/no-literal-string': 'off'
+                'i18next/no-literal-string': 'off',
+                'max-len': 'off'
             }
         }
     ]
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
 import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 interface NavbarProps {
     className?: string
@@ -10,6 +12,12 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
             <div className={cls.links}>
@@ -19,6 +27,17 @@ export const Navbar = ({ className }: NavbarProps) => {
                 <AppLink theme={AppLinkTheme.RED} to={'/about'}>
                     {t('О сайте')}
                 </AppLink>
+                <Button
+                    theme={ButtonTheme.CLEAR_INVERTED}
+                    className={cls.links}
+                    onClick={onToggleModal}
+                >
+                    {t('Войти')}
+                </Button>
+                <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                    {/* eslint-disable-next-line */}
+                    {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.')}
+                </Modal>
             </div>
         </div>
     )
